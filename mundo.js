cc8cfb60c694f246278b0cec4e9d0d3a1f39cd82
ANTONIA FIBLA <antonia.fibla@uc.cl>
2024-11-09 18:11:36 -0300

    sugerencias profe

--- mundo.js
+++ mundo.js
@@ -1,58 +1,76 @@
 const continentSelect = document.getElementById('continentSelect');
 const myPlot = document.getElementById('myMap');
-const svgImage = document.getElementById('context'); // Referencia a la imagen SVG
+const coastersList = document.getElementById('coastersList');  // Contenedor de la lista de montañas rusas
+const hoverInfo = document.getElementById('hoverInfo'); // Contenedor de la información al pasar el cursor
+let currentHoverIndex = null; // Variable para rastrear el índice actual de hover
+
 
 function updateMap(continent) {
     let filteredData;
 
-    // Filtrar datos según el continente o "World"
     if (continent === "World") {
         filteredData = coasterData.slice().sort((a, b) => b.Speed - a.Speed);
-        svgImage.style.display = "block"; // Mostrar la imagen SVG
     } else {
         filteredData = coasterData
             .filter(coaster => coaster.Continent === continent)
             .sort((a, b) => b.Speed - a.Speed)
             .slice(0, 10);
-        svgImage.style.display = "none"; // Ocultar la imagen SVG
     }
 
-    // Extraer coordenadas, velocidad y nombres para el mapa
+    coastersList.innerHTML = '<h2>Top 10</h2>';
+    listItems = [];
+
+    filteredData.slice(0, 10).forEach((coaster, index) => {
+        const listItem = document.createElement('div');
+        listItem.innerHTML = `<strong>${index + 1}° ${coaster.coaster_name}</strong>`;
+        listItem.classList.add('list-item');
+        listItems.push(listItem); // Almacenar referencia del elemento
+
+        // Evento de mouseover para la lista
+        listItem.addEventListener('mouseover', () => {
+            // Cambiar color del marcador y mostrar información
+            listItem.style.color = 'red';
+            highlightMarker(index);
+            
+        });
+
+        // Evento de mouseout para la lista
+        listItem.addEventListener('mouseout', () => {
+            listItem.style.color = '';
+            resetHighlight();
+            
+        });
+
+        coastersList.appendChild(listItem);
+    });
+
     const lon = filteredData.map(item => item.Lon);
     const lat = filteredData.map(item => item.Lat);
-    const nombre = filteredData.map(item => item.coaster_name);
-
-    // Definir el factor de escala en función del continente
     let speedFactor;
     switch (continent) {
-        case "north america":
-            speedFactor = 0.2; break;
-        case "south america":
-            speedFactor = 0.35; break;
-        case "oceania":
-            speedFactor = 0.3; break;
-        case "europe":
-            speedFactor = 0.3; break;
-        case "asia":
-            speedFactor = 0.2; break;
-        case "africa":
-            speedFactor = 0.4; break;
-        default:
-            speedFactor = 0.15; break;
+        case "north america": speedFactor = 0.2; break;
+        case "south america": speedFactor = 0.35; break;
+        case "oceania": speedFactor = 0.3; break;
+        case "europe": speedFactor = 0.3; break;
+        case "asia": speedFactor = 0.2; break;
+        case "africa": speedFactor = 0.4; break;
+        default: speedFactor = 0.15; break;
     }
 
-    // Aplicar el factor de escala a cada velocidad
     const magnitud = filteredData.map(item => item.Speed * speedFactor);
-
-    // Configuración de datos de Plotly
     const data = [
         {
             type: 'scattergeo',
             mode: 'markers',
             lon: lon,
             lat: lat,
-            text: filteredData.map(item =>
-                `${item.coaster_name}<br>Location: ${item.Location}<br>Park: ${item.Park}<br>Speed: ${item.Speed} km/h`
+            text: filteredData.map(item => item.coaster_name),
+            customdata: filteredData.map((item, index) =>
+                `<b>${item.coaster_name}</b><br>
+                <span style="color: gray;">Ranking:</span> ${index + 1}<br>
+                <span style="color: gray;">Ubicación:</span> ${item.Location}<br>
+                <span style="color: gray;">Parque:</span> ${item.Park}<br>
+                <span style="color: red;">Velocidad:</span> ${item.Speed} km/h`
             ),
             hoverinfo: 'text',
             marker: {
@@ -60,8 +78,7 @@ function updateMap(continent) {
                 size: magnitud,
                 colorscale: [[0, '#fff'], [1, '#000']],
                 line: { color: 'black' }
-            },
-            customdata: filteredData.map(item => item.Speed) // Añadir la velocidad como customdata
+            }
         }
     ];
 
@@ -71,6 +88,7 @@ function updateMap(continent) {
             scope: 'world',
             projection: { type: 'natural earth' },
             showland: false,
+            // showcountries: true,
             borderrwidth: 1,
             showframe: false,
             lataxis: { range: [-60, 90] }
@@ -99,7 +117,7 @@ function updateMap(continent) {
             projection: { type: 'natural earth' },
             showland: true,
             showcountries: true,
-            landcolor: 'rgb(243,243,243)',
+            landcolor: 'rgba(0,0,0,0)',
             countrycolor: 'rgb(119, 119, 119)',
             countrywidth: 0.5,
             coastlinecolor: 'rgba(0,0,0,0)',
@@ -109,15 +127,48 @@ function updateMap(continent) {
         dragmode: false
     };
 
-    // Renderizar el mapa con los datos y el layout
+
     Plotly.newPlot(myPlot, data, layout, { scrollZoom: true, displayModeBar: false });
 
-    // Evento para reproducir sonido al hacer clic en un marcador
-    myPlot.on('plotly_click', function(data) {
-        const point = data.points[0];
-        const speed = point.customdata; // Obtiene la velocidad de la montaña rusa
-        playSoundForSpeed(speed); // Llama a la función de sonificación en sonification.js
+    // Evento plotly_hover para el mapa
+    myPlot.on('plotly_hover', function(data) {
+        const pointIndex = data.points[0].pointIndex;
+        currentHoverIndex = pointIndex;
+        highlightMarker(pointIndex);
     });
+
+    // Evento plotly_unhover para el mapa
+    myPlot.on('plotly_unhover', function() {
+        resetHighlight();
+    });
+    
+    // Función para resaltar marcador y mostrar información
+    function highlightMarker(index) {
+        // Verifica si el índice corresponde a un elemento en la lista
+        if (listItems[index]) {
+            listItems[index].style.color = 'red'; // Cambia el color del nombre en la lista si existe
+        }
+        
+        const newColors = magnitud.slice();
+        newColors[index] = 'red'; // Cambia el color del marcador en el mapa
+        Plotly.restyle(myPlot, { 'marker.color': [newColors] });
+        hoverInfo.innerHTML = data[0].customdata[index];
+    }
+    
+    // Función para restaurar color de marcadores y limpiar información
+    function resetHighlight() {
+        if (currentHoverIndex !== null) {
+            // Restaura el color del nombre en la lista si el índice está dentro de listItems
+            if (listItems[currentHoverIndex]) {
+                listItems[currentHoverIndex].style.color = '';
+            }
+            
+            const resetColors = magnitud.slice();
+            Plotly.restyle(myPlot, { 'marker.color': [resetColors] });
+            hoverInfo.innerHTML = '';
+            currentHoverIndex = null;
+        }
+    }
 }
 
 // Evento de cambio en el selector de continente
@@ -127,3 +178,89 @@ continentSelect.addEventListener('change', function() {
 
 // Inicializar el mapa con la opción "World"
 updateMap("World");
+
+
+
+
+    // // Configuración específica del layout
+    // const layout = (continent === "World") ? {
+    //     geo: {
+    //         scope: 'world',
+    //         projection: { type: 'natural earth' },
+    //         showland: false,
+    //         // showcountries: true,
+    //         borderrwidth: 1,
+    //         showframe: false,
+    //         lataxis: { range: [-60, 90] }
+    //     },
+    //     margin: { l: 0, r: 0, b: 0, t: 0, pad: 0 },
+    //     dragmode: false
+    // } : (continent === "oceania") ? {
+    //     geo: {
+    //         scope: 'world',
+    //         projection: { type: 'natural earth' },
+    //         showland: true,
+    //         showcountries: true,
+    //         landcolor: 'rgb(243,243,243)',
+    //         countrycolor: 'rgb(119, 119, 119)',
+    //         countrywidth: 0.5,
+    //         coastlinecolor: 'rgba(0,0,0,0)',
+    //         showframe: false,
+    //         lataxis: { range: [-40, -10] },
+    //         lonaxis: { range: [100, 170] }
+    //     },
+    //     margin: { l: 0, r: 0, b: 0, t: 0, pad: 0 },
+    //     dragmode: false
+    // } : {
+    //     geo: {
+    //         scope: continent,
+    //         projection: { type: 'natural earth' },
+    //         showland: true,
+    //         showcountries: true,
+    //         landcolor: 'rgb(243,243,243)',
+    //         countrycolor: 'rgb(119, 119, 119)',
+    //         countrywidth: 0.5,
+    //         coastlinecolor: 'rgba(0,0,0,0)',
+    //         showframe: false,
+    //     },
+    //     margin: { l: 0, r: 0, b: 0, t: 0, pad: 0 },
+    //     dragmode: false
+    // };
+
+
+
+    // const data = [
+    //     {
+    //         type: 'scattergeo',
+    //         mode: 'markers',
+    //         lon: filteredData.map(item => item.Lon),
+    //         lat: filteredData.map(item => item.Lat),
+    //         text: filteredData.map(item => item.coaster_name),
+    //         customdata: filteredData.map(item =>
+    //             `<b>${item.coaster_name}</b><br>
+    //             <span style="color: gray;">Ubicación:</span> ${item.Location}<br>
+    //             <span style="color: gray;">Parque:</span> ${item.Park}<br>
+    //             <span style="color: red;">Velocidad:</span> ${item.Speed} km/h`
+    //         ),
+    //         hoverinfo: 'text',
+    //         marker: {
+    //             color: magnitud,
+    //             size: magnitud,
+    //             colorscale: [[0, '#fff'], [1, '#000']],
+    //             line: { color: 'black' }
+    //         }
+    //     }
+    // ];
+
+    // let speedFactor;
+    // switch (continent) {
+    //     case "north america": speedFactor = 0.2; break;
+    //     case "south america": speedFactor = 0.35; break;
+    //     case "oceania": speedFactor = 0.3; break;
+    //     case "europe": speedFactor = 0.3; break;
+    //     case "asia": speedFactor = 0.2; break;
+    //     case "africa": speedFactor = 0.4; break;
+    //     default: speedFactor = 0.15; break;
+    // }
+
+    // const magnitud = filteredData.map(item => item.Speed * speedFactor);
